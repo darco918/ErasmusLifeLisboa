1cd815c20076e698990314d12389e3d1231f95f7
Darco Murja <andrei.tugmeanu@gmail.com>
2022-02-25 00:16:49 +0200

    back camera intent

--- app/src/main/java/com/example/easmuslifeapp/UserDataActivity.kt
+++ app/src/main/java/com/example/easmuslifeapp/UserDataActivity.kt
@@ -1,12 +1,17 @@
 package com.example.easmuslifeapp
 
+import android.app.Activity
 import android.app.Dialog
 import android.content.Intent
+import android.graphics.Bitmap
 import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
 import android.view.Window
 import android.widget.Toast
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 import com.example.easmuslifeapp.databinding.ActivityUserDataBinding
 import com.example.easmuslifeapp.datamodels.User
 import com.google.firebase.auth.FirebaseAuth
@@ -23,6 +28,8 @@ class UserDataActivity : AppCompatActivity() {
     private lateinit var storageReference: StorageReference
     private lateinit var imageUri: Uri
     private lateinit var dialog: Dialog
+    private lateinit var profileImage: de.hdodenhof.circleimageview.CircleImageView
+    private lateinit var resultLauncher: ActivityResultLauncher<Intent>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -31,6 +38,19 @@ class UserDataActivity : AppCompatActivity() {
         auth = FirebaseAuth.getInstance()
         val uid = auth.currentUser?.uid
         dataBaseReference = FirebaseDatabase.getInstance().getReference("Users")
+
+        profileImage = findViewById(R.id.profile_image)
+        profileImage.setOnClickListener {
+            val cameraIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+            resultLauncher.launch(cameraIntent)
+        }
+
+        resultLauncher =
+            registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+                if (result.resultCode == Activity.RESULT_OK) {
+                    handleCameraImage(result.data)
+                }
+            }
         binding.saveBtn.setOnClickListener {
             startActivity(Intent(this, MainActivity2::class.java))
 //            showProgressBar()
@@ -51,13 +71,19 @@ class UserDataActivity : AppCompatActivity() {
         }
     }
 
+    private fun handleCameraImage(intent: Intent?) {
+        val bitmap = intent?.extras?.get("data") as Bitmap
+        profileImage.setImageBitmap(bitmap)
+    }
+
     private fun uploadProfilePicture() {
         imageUri = Uri.parse("android.resource://$packageName/${R.drawable.profile}")
-        storageReference = FirebaseStorage.getInstance().getReference("Users/" + auth.currentUser?.uid)
-        storageReference.putFile(imageUri).addOnCompleteListener{
+        storageReference =
+            FirebaseStorage.getInstance().getReference("Users/" + auth.currentUser?.uid)
+        storageReference.putFile(imageUri).addOnCompleteListener {
             hideProgressBar()
             Toast.makeText(this, "Uploaded image successfully", Toast.LENGTH_LONG).show()
-        }.addOnFailureListener{
+        }.addOnFailureListener {
             hideProgressBar()
             Toast.makeText(this, "Failed to upload image", Toast.LENGTH_LONG).show()
         }
